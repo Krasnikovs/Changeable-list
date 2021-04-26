1c4d896193801769c56a8c278103ef4c741d553e
Krasnikovs <ralis3333@gmail.com>
2021-04-26 18:37:51 +0300

    List placement

--- script.js
+++ script.js
@@ -3,6 +3,30 @@ let list = [];
 document.getElementById('buttonAdd').addEventListener('click', () => {
     console.log('Nospiests');
 
-    console.log(job.value);
-    console.log(time.value);
+    let fullJob = {job: job.value, time: time.value};
+
+    job.value = "";
+    time.value = "";
+
+    list.push(fullJob);
+
+    console.log(list);
+    
+    render();
 });
+
+function render() {
+    let Jobs = document.getElementById('list')
+    Jobs.innerHTML = "";
+
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i]);
+         let fullJob = `
+            <div class="Jobs">
+                <p>Uzdevums: ${list[i].job}</p>
+                <p>Paredētais laiks: ${list[i].time}</p>
+            </div>`
+
+        Jobs.innerHTML += fullJob;
+    }
+};
